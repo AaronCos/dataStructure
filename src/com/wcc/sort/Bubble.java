72811044b49b9d76575340d19ae9f2a7b02b61a0
Aaron <miracle_lc@163.com>
2017-09-13 11:53:43 +0800

    change a comment

--- src/com/wcc/sort/Bubble.java
+++ src/com/wcc/sort/Bubble.java
@@ -6,7 +6,7 @@ public class Bubble {
 	public static void main(String[] args) {
 		int a[] = {1,32,321,4,5,3,49,32,22,13,24,45};
 		
-		//冒泡排序法
+		
 		for(int i=0;i<a.length-1;i++){//外层循环控制趟数
 			for(int j=0;j<a.length-1-i;j++){//内层控制前后两项的交换，每轮循环开始都是从第0位开始
 				if(a[j]>a[j+1]){
